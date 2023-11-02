47e59db64151547499309fd5391a36e97971b68b
DanteBoute <danteboute1@outlook.com>
2023-11-02 14:42:08 +0100

    Finish step 1 of project 5 'events'

--- 1.Introduction/script.js
+++ 1.Introduction/script.js
@@ -1,33 +1,51 @@
-document.title = "Modifying the DOM"
-console.log(document.title)
+// document.title = "Modifying the DOM"
+// console.log(document.title)
 
 
-// function RGBcolor() {
-//     let R = Math.floor(Math.random() * 256);
-//     let G = Math.floor(Math.random() * 256);
-//     let B = Math.floor(Math.random() * 256);
-//     let randomcolor = "rgb(" + R + "," + G + "," + B + ")";  
-//     console.log(randomcolor);
-//     return randomcolor;
-//   }
+// // function RGBcolor() {
+// //     let R = Math.floor(Math.random() * 256);
+// //     let G = Math.floor(Math.random() * 256);
+// //     let B = Math.floor(Math.random() * 256);
+// //     let randomcolor = "rgb(" + R + "," + G + "," + B + ")";  
+// //     console.log(randomcolor);
+// //     return randomcolor;
+// //   }
   
  
-//   document.body.style.backgroundColor = RGBcolor();
+// //   document.body.style.backgroundColor = RGBcolor();
 
-// // Get a reference to the <body> element
-// let body = document.body;
+// // // Get a reference to the <body> element
+// // let body = document.body;
 
-// // Get all the children elements of the <body>
-// let children = body.children;
+// // // Get all the children elements of the <body>
+// // let children = body.children;
 
-// // Convert the children collection to an array for easier manipulation
-// let childrenArray = Array.from(children);
+// // // Convert the children collection to an array for easier manipulation
+// // let childrenArray = Array.from(children);
 
-// // Loop through the children and display them
-// childrenArray.forOf(childOfBody => {
-//   console.log(childOfBody);
+// // // Loop through the children and display them
+// // childrenArray.forOf(childOfBody => {
+// //   console.log(childOfBody);
 
-// });
-for (const child of document.body.children) {
-    console.log(child)
+// // });
+// for (const child of document.body.children) {
+//     console.log(child)
+// }
+
+const secondChild = document.querySelector('body');
+// console.log(secondChild);
+// secondChild.style.backgroundColor = ('#FF69B4');
+
+function RGBcolor() {
+    let R = Math.floor(Math.random() * 256);
+
+    let G = Math.floor(Math.random() * 256);
+
+    let B = Math.floor(Math.random() * 256);
+
+    let randomcolor = "rgb(" + R + "," + G + "," + B + ")";
+    console.log(randomcolor);
 }
+RGBcolor();
+
+secondChild.style.backgroundColor = RGBcolor();
--- 5.Events/script.js
+++ 5.Events/script.js
@@ -4,13 +4,36 @@ const getElapsedTime = () => {
   return Number((Date.now() - _initTime) / 1000).toFixed(2) + 's'
 }
 
+
 const clickOnSquare = (e) => {
   console.log(e.target.classList[1])
   console.log(getElapsedTime())
+  const newDiv = document.createElement('div');
+  const targetCloneDiv = document.querySelector('.displayedsquare-wrapper');
+  newDiv.className = ('displayedsquare '+e.target.classList[1]);
+  targetCloneDiv.appendChild(newDiv);
 }
-
 const actionSquares = document.querySelectorAll('.actionsquare')
 for (let actionSquare of actionSquares) {
   actionSquare.addEventListener('click', clickOnSquare)
 }
+
+  
+
+// function cloneSquare(){
+//   const originalSquare = document.querySelector('.actionsquare');
+//   const clonedSquare = originalSquare.cloneNode(false);
+//   const targetCloneDiv = document.querySelector('.displayedsquare-wrapper');
+//   targetCloneDiv.appendChild(clonedSquare);
+// }
+
+// originalSquare.addEventListener('click', function() {
+//   const className = square.className;
+//   console.log('Class Name:', className);
+// })
+
+// TO DO: Every time a square is clicked:
+//        Create a new <div> with class with the same color
+
+// create a new div
 
