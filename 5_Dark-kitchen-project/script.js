7f4d3824bf376583b6c2ca58eab60ac2d09b1c3f
DanteBoute <danteboute1@outlook.com>
2023-11-07 11:59:52 +0100

    Add a dark mode switch and fix of the cart

--- 5_Dark-kitchen-project/script.js
+++ 5_Dark-kitchen-project/script.js
@@ -2,16 +2,16 @@ const pizzas = [
     {
       name: 'Pepperoni Pizza',
       crust: 'Thin Crust',
-      toppings: ['Pepperoni', 'Mozzarella Cheese', 'Tomato Sauce'],
-      price: '€' + 12.99,
+      toppings: ['Pepperoni', ' Mozzarella', ' Tomato Sauce'],
+      price:'€' + 12.99,
       picture: 'pizzas 2/1.png',
       vegetarian: 'No'
     },
     {
       name: 'Margherita Pizza',
       crust: 'Thin Crust',
-      toppings: ['Fresh Basil', 'Mozzarella Cheese', 'Tomato Sauce'],
-      price: '€' + 10.99,
+      toppings: ['Fresh Basil', ' Mozzarella', ' Tomato Sauce'],
+      price:'€' + 10.99,
       picture: 'pizzas 2/2.png',
       vegetarian: 'Yes'
 
@@ -19,8 +19,8 @@ const pizzas = [
     {
       name: 'Supreme Pizza',
       crust: 'Pan Crust',
-      toppings: ['Pepperoni', 'Sausage', 'Bell Peppers', 'Onions', 'Olives'],
-      price: '€' + 15.99,
+      toppings: ['Pepperoni', ' Sausage', ' Bell Peppers', ' Onions', ' Olives'],
+      price:'€' + 15.99,
       picture: 'pizzas 2/3.png',
       vegetarian: 'No'
 
@@ -28,8 +28,8 @@ const pizzas = [
     {
       name: 'Vegetarian Pizza',
       crust: 'Thin Crust',
-      toppings: ['Mushrooms', 'Green Peppers', 'Olives', 'Tomato Sauce'],
-      price: '€' + 11.99,
+      toppings: ['Mushrooms', ' Green Peppers', ' Olives', ' Tomato Sauce'],
+      price:'€' + 11.99,
       picture: 'pizzas 2/4.png',
       vegetarian: 'Yes'
 
@@ -37,8 +37,8 @@ const pizzas = [
     {
       name: 'Hawaiian Pizza',
       crust: 'Thin Crust',
-      toppings: ['Ham', 'Pineapple', 'Mozzarella Cheese', 'Tomato Sauce'],
-      price: '€' + 10.99,
+      toppings: ['Ham', ' Pineapple', ' Mozzarella', ' Tomato Sauce'],
+      price:'€' + 10.99,
       picture: 'pizzas 2/5.png',
       vegetarian: 'No'
 
@@ -46,8 +46,8 @@ const pizzas = [
     {
         name: 'BBQ Chicken Pizza',
         crust: 'Thin Crust',
-        toppings: ['Grilled Chicken', 'BBQ Sauce', 'Red Onions', 'Cilantro'],
-        price: '€' + 13.99,
+        toppings: ['Grilled Chicken', ' BBQ Sauce', ' Red Onions', ' Cilantro'],
+        price:'€' + 13.99,
         picture: 'pizzas 2/6.png',
         vegetarian: 'No'
 
@@ -55,8 +55,8 @@ const pizzas = [
       {
         name: 'Mushroom Lovers Pizza',
         crust: 'Pan Crust',
-        toppings: ['Mushrooms', 'Mozzarella', 'Parmesan', 'Tomato Sauce'],
-        price: '€' + 11.99,
+        toppings: ['Mushrooms', ' Mozzarella', ' Parmesan', ' Tomato Sauce'],
+        price:'€' + 11.99,
         picture: 'pizzas 2/7.png',
         vegetarian: 'Yes'
 
@@ -64,8 +64,8 @@ const pizzas = [
       {
         name: 'Meat Lovers Pizza',
         crust: 'Pan Crust',
-        toppings: ['Pepperoni', 'Sausage', 'Bacon', 'Ham', 'Ground Beef'],
-        price: '€' + 16.99,
+        toppings: ['Pepperoni', ' Sausage', ' Bacon', ' Ham', ' Ground Beef'],
+        price:'€' + 16.99,
         picture: 'pizzas 2/8.png',
         vegetarian: 'No'
 
@@ -73,8 +73,8 @@ const pizzas = [
       {
         name: 'White Pizza',
         crust: 'Thin Crust',
-        toppings: ['Ricotta', 'Mozzarella', 'Garlic', 'Spinach'],
-        price: '€' + 12.99,
+        toppings: ['Ricotta', ' Mozzarella', ' Garlic', ' Spinach'],
+        price:'€' + 12.99,
         picture: 'pizzas 2/9.png',
         vegetarian: 'Yes'
 
@@ -82,8 +82,8 @@ const pizzas = [
       {
         name: 'Buffalo Chicken Pizza',
         crust: 'Thin Crust',
-        toppings: ['Buffalo Chicken', 'Mozzarella', 'Red Onions', 'Blue Cheese'],
-        price: '€' + 10.99,
+        toppings: ['Buffalo Chicken', ' Mozzarella', ' Red Onions', ' Blue Cheese'],
+        price:'€' + 10.99,
         picture: 'pizzas 2/10.png',
         vegetarian: 'No'
 
@@ -91,8 +91,8 @@ const pizzas = [
       {
         name: 'Vegetable Delight Pizza',
         crust: 'Thin Crust',
-        toppings: ['Broccoli', 'Mushrooms', 'Bell Peppers', 'Tomato Sauce'],
-        price: '€' + 11.49,
+        toppings: ['Broccoli', ' Mushrooms', ' Bell Peppers', 'Tomato Sauce'],
+        price:'€' + 11.49,
         picture: 'pizzas 2/3.png',
         vegetarian: 'Yes'
 
@@ -100,14 +100,13 @@ const pizzas = [
       {
         name: 'Four Cheese Pizza',
         crust: 'Pan Crust',
-        toppings: ['Mozzarella', 'Cheddar', 'Parmesan', 'Ricotta', 'Tomato Sauce'],
-        price: '€' + 13.99,
+        toppings: ['Mozzarella', ' Cheddar', ' Parmesan', ' Ricotta', ' Tomato Sauce'],
+        price:'€' + 13.99,
         picture: 'pizzas 2/5.png',
         vegetarian: 'Yes'
 
       }
   ];
-
   const mainSection = document.querySelector('main');
 
   const onePizza = (onePizza) =>
@@ -149,13 +148,13 @@ const pizzas = [
   pizzas.forEach(pizza => {
     onePizza(pizza);
   });
-
   const filterVegetarian = (pizzas)  => {
     let vegetarianPizzas = pizzas.filter(pizza => pizza.vegetarian === 'Yes');
     mainSection.innerHTML = '';
     vegetarianPizzas.forEach(vegPizza => {
         onePizza(vegPizza);
     })
+    setupAddToCartListeners();
   }
 const veggieButton = document.querySelector('.veggieButton');
 veggieButton.addEventListener('click', () => {filterVegetarian(pizzas)});
@@ -166,7 +165,9 @@ const filterThinCrust = (pizzas)  => {
     thinCrustPizzas.forEach(thinPizza => {
         onePizza(thinPizza);
     })
+    setupAddToCartListeners();
   }
+  
 const thinCrustButton = document.querySelector('.thinCrustButton');
 thinCrustButton.addEventListener('click', () => {filterThinCrust(pizzas)});
 
@@ -176,6 +177,7 @@ const filterPanCrust = (pizzas)  => {
     PanCrustPizzas.forEach(panPizza => {
         onePizza(panPizza);
     })
+    setupAddToCartListeners();
   }
 const panCrustButton = document.querySelector('.panCrustButton');
 panCrustButton.addEventListener('click', () => {filterPanCrust(pizzas)});
@@ -184,27 +186,49 @@ const AllPizzasButton = document.querySelector('.allPizzasButton');
 AllPizzasButton.addEventListener('click', () => {  
     mainSection.innerHTML = '';
     pizzas.forEach(pizza => {
-    onePizza(pizza);})})
+    onePizza(pizza)
+    
+    })
+    setupAddToCartListeners();
+  });
 
 // ADD TO CART
 
-document.addEventListener("DOMContentLoaded", function() {
+// document.addEventListener("DOMContentLoaded", function() {
+function setupAddToCartListeners() {
   const addToCartButtons = document.querySelectorAll("button");
   const cartItemList = document.getElementById("cart-items");
+  const totalAmountElement = document.getElementById("total-amount");
+  let totalAmount = 0;
 
   addToCartButtons.forEach(button => {
     button.addEventListener("click", function() {
       const pizzaName = this.parentElement.querySelector("h2").textContent;
-      const pizzaPrice = this.parentElement.querySelector(".priceOfPizza").textContent;
+      const pizzaPrice = parseFloat(this.parentElement.querySelector(".priceOfPizza").textContent.replace("€", ""));
 
       const cartItem = document.createElement("li");
-      cartItem.textContent = `${pizzaName} - ${pizzaPrice}`;
+      cartItem.textContent =`${pizzaName} - ${pizzaPrice}`;
 
       cartItemList.appendChild(cartItem);
-
-      console.log(cartItem);
+      
+      totalAmount += pizzaPrice;
+      totalAmountElement.textContent = `${totalAmount}`;
+      
     });
-  })
-})
+  });
+}
+setupAddToCartListeners();
 
-  
+document.addEventListener("DOMContentLoaded", function() {
+  const darkModeToggle = document.getElementById("dark-mode-toggle");
+
+  darkModeToggle.addEventListener("change", function() {
+      const isDarkMode = this.checked;
+
+      if (isDarkMode) {
+          document.body.classList.add("dark-mode");
+      } else {
+          document.body.classList.remove("dark-mode");
+      }
+  });
+});
